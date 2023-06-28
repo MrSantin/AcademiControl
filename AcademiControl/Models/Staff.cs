5c8fd290e2d73bb6bc38f963dc87770968be02db
BiaDrumm07 <bianca.drumm@hotmail.com>
2023-06-27 22:09:19 -0300

    Atualizações nas labels
    Atualizações nas labels do Staff

--- AcademiControl/Models/Staff.cs
+++ AcademiControl/Models/Staff.cs
@@ -1,10 +1,19 @@
-﻿namespace AcademiControl.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace AcademiControl.Models
 {
     public class Staff : IEntity
     {
         public Guid Id { get; set; }
+
+        [Display(Name = "Nome")]
         public string Name { get; set; }
+
+        [Display(Name = "E-mail")]
         public string Email { get; set; }
+
+        [Display(Name = "Celular")]
         public string Phone { get; set; }
     }
 }

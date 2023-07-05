ba1d19fbbdb4aa9be46a1c219cca4628620dfa31
Marcos Santin <mrsantin.ms@gmail.com>
2023-07-05 19:34:38 -0300

    feat: Adiciona scripts de create e edit para a entidade Project

--- AcademiControl/wwwroot/js/Projects/Edit.js
+++ AcademiControl/wwwroot/js/Projects/Edit.js
@@ -1,15 +1,16 @@
-﻿$(document).ready(function () {
+﻿function Edit(projectId) {
     var btnSubmit = $("#btnSubmit");
     btnSubmit.on("click", function () {
         var command = {
             ProjectName: $('#Name').val(),
             ProjectDescription: $('#Description').val(),
-            ProjectOwner: $('#OwnerId').val()
+            ProjectOwner: $('#OwnerId').val(),
+            Id: projectId
         };
         console.log(command);
         console.log(JSON.stringify(command));
         $.ajax({
-            url: '/Projects/Create',
+            url: '/Projects/Edit',
             method: "POST",
             contentType: "application/json",
             data: JSON.stringify(command),
@@ -18,7 +19,20 @@
             },
             error: function (error) {
                 console.log(error);
-                alert("não foi possível salvar o registro");            }
+                alert("não foi possível salvar o registro");
+            }
         });
     });
-});
+};
+
+function selectPO(poId) {
+
+    $("#OwnerId option").each(function () {
+        console.log($(this).val());
+        if ($(this).val() === poId) {
+           
+            $(this).attr("selected", "selected");
+            return false; // Sai do loop quando a opção correta for encontrada
+        }
+    });
+};
